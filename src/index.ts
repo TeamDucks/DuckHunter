6b6c700d22f92eb3451dfc893c63f38b2c25aaeb
Huy Le <huy.lenq@gmail.com>
2016-07-24 17:42:05 +0700

    Group + a little bit of refactor

--- src/index.ts
+++ src/index.ts
@@ -13,38 +13,39 @@ function preload() {
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
-function create() {
-    var platforms: Phaser.Group;
-
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //  A simple background for our game
+function createEnvironment() {
     game.add.sprite(0, 0, 'sky');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
-
-    //  We will enable physics for any object that is created in this group
+    var platforms = game.add.group();
     platforms.enableBody = true;
 
-    // Here we create the ground.
     var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
 
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
+    var ledge1 = platforms.create(400, 400, 'ground');
+    ledge1.body.immovable = true;
 
-    ledge.body.immovable = true;
+    var ledge2 = platforms.create(-150, 250, 'ground');
+    ledge2.body.immovable = true;
+}
 
-    ledge = platforms.create(-150, 250, 'ground');
+function createPlayer() {
+    var player = game.add.sprite(32, game.world.height - 150, 'dude');
+    game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
 
-    ledge.body.immovable = true;
+    //  Our two animations, walking left and right.
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+}
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    createEnvironment();
+    createPlayer();
 }
 
 function update() {
